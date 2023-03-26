24648389fe0e9f3d4fb2b245173f39838694442a
NIKKITALSG <superlaim6@gmail.com>
2023-03-27 00:27:12 +0300

    test for tigr

--- src/main/java/org/example/Auto.java
+++ src/main/java/org/example/Auto.java
@@ -1,13 +1,43 @@
 package org.example;
 
 public class Auto {
+    private String Model = "Silvia";
+    private String Name ="Nissan Silvia";
+    private int Age = 1998;
+    private int Price = 700000;
 
-        String _name = "Порше";
-        String _model = "911";
-        int _age = 2013;
+    public String _getModel(){
+        return Model;
+    }
+    public void _setModel(String _changeModel){
+        Model = _changeModel;
+    }
+    public String _getName(){
+        return Name;
+    }
+    public  void _setName(String _changeName){
+        Name = _changeName;
+    }
+    public int _getAge(){
+        return Age;
+    }
+    public void  _setAge(Integer _changeAge){
+        Age = _changeAge;
+    }
+    public int _getPrice(){
+        return Price;
+    }
+    public void _setPrice(Integer _changePrice){
+        Price = _changePrice;
+    }
 
     @Override
     public String toString() {
-        return "Text " + _model +" "+ _name +" "+ _age  ;
+        return  "Auto " + '\'' +
+                "Model= " + Model + '\'' +
+                "Name= " + Name + '\'' +
+                "Age= " + Age + '\'' +
+                "Price= " + Price ;
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
-import org.example.Nissan;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Auto check = new Auto();
-        Nissan name = new Nissan();
-        Porshe name1 = new Porshe();
-        Toyota neme2 = new Toyota();
+
+        Auto name = new Auto();
+
+
 
       }
 
